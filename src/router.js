6d755bc9e6dd552177c126589253992f14778cc4
kuru <amohkuru@gmail.com>
2019-12-08 04:17:24 +0900

    ルーティング追加

--- src/router.js
+++ src/router.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import GenreIndex from './views/genre/GenreIndex.vue'
+import GenreCreate from './views/genre/GenreCreate.vue'
+import GenreEdit from './views/genre/GenreEdit.vue'
+import GenreDetail from './views/genre/GenreDetail.vue'
+import TopicIndex from './views/topic/TopicIndex.vue'
+import TopicDetail from './views/topic/TopicDetail.vue'
 import TaskIndex from './views/task/TaskIndex.vue'
+import TaskDetail from './views/task/TaskDetail.vue'
 
 
 import FieldIndex from './views/field/FieldIndex.vue'
@@ -22,14 +28,51 @@ Vue.use(Router)
 export default new Router({
     mode: 'history',
     base: process.env.BASE_URL,
-    routes: [{
+    routes: [
+        //top
+        {
             path: '/',
             component: GenreIndex
         },
+        //genre
+        {
+            path: '/genres',
+            component: GenreIndex
+        },
+        {
+            path: '/genres/create',
+            component: GenreCreate
+        },
+        {
+            path: '/genres/:label/edit',
+            component: GenreEdit
+        },
+        {
+            path: '/genres/:label/topics',
+            component: GenreDetail
+        },
+        //topic
+        {
+            path: '/topics',
+            component: TopicIndex
+        },
+        {
+            path: '/topics/:topic_id/tasks',
+            component: TopicDetail
+        },
+        //tasks
         {
             path: '/tasks',
             component: TaskIndex
         },
+        {
+            path: '/tasks/:task_id',
+            component: TaskDetail
+        },
+
+
+
+
         //field
         {
             path: '/field/index',
