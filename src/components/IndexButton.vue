a000e321c761959d19605a53c5c4351d8e02d070
kuru <amohkuru@gmail.com>
2019-12-08 04:14:07 +0900

    サブヘッダー追加

---
+++ src/components/IndexButton.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row>
+        <v-col
+        cols="auto"
+        class="mr-auto"
+        >
+        <div class="display-1 font-weight-thin">精進記録</div>
+        </v-col>
+        <v-col cols="auto">
+        <v-btn rounded outlined color="purple darken-4" to="/genres">genres</v-btn>
+        <v-btn rounded outlined color="purple darken-4" to="/topics">topics</v-btn>
+        <v-btn rounded outlined color="purple darken-4" to="/tasks">tasks</v-btn>
+        </v-col>
+    </v-row>
+  </v-container>
+</template>
+
