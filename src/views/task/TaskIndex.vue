133aac202f440210e1db077fa105e96d191e5ccf
ningenMe <amohkuru@gmail.com>
2020-11-14 04:44:21 +0900

    [add] topic indexを再開

--- src/views/task/TaskIndex.vue
+++ src/views/task/TaskIndex.vue
@@ -1,87 +1,105 @@
 <template>
-  <v-container>
+<v-container>
 
     <!-- edit,create button -->
     <v-row>
-      <v-col
-        cols="auto"
-        class="mr-auto"
-      >
-        <v-btn outlined color="yellow darken-3" disabled >task</v-btn>
-        <v-btn outlined color="yellow darken-3">
-        <select v-model="task">
-          <option v-bind:value="task" v-for="(task,i) in tasks" v-bind:key="i">
-            {{ task.taskName}}
-          </option>
-        </select>
-        ▼
-        </v-btn>
-        <v-btn outlined color="yellow darken-3" v-bind:to = "'/tasks/' + task.taskId + '/edit'" v-if="$store.getters['getAccessToken'] != null">edit</v-btn>
-        <v-btn outlined color="yellow darken-3" v-bind:to = "'/tasks/' + task.taskId" v-if="$store.getters['getAccessToken'] == null">tasks</v-btn>
+        <v-col cols="auto" class="mr-auto">
+            <v-btn outlined color="yellow darken-3" disabled>task</v-btn>
+            <v-btn outlined color="yellow darken-3">
+                <select v-model="task">
+                    <option v-bind:value="task" v-for="(task,i) in tasks" v-bind:key="i">
+                        {{ task.taskName}}
+                    </option>
+                </select>
+                ▼
+            </v-btn>
+            <v-btn outlined color="yellow darken-3" v-bind:to="'/tasks/' + task.taskId + '/edit'" v-if="$store.getters['getAccessToken'] != null">edit</v-btn>
+            <v-btn outlined color="yellow darken-3" v-bind:to="'/tasks/' + task.taskId" v-if="$store.getters['getAccessToken'] == null">tasks</v-btn>
 
-      </v-col>
-      <v-col cols="auto">
-        <v-btn outlined color="light-blue accent-4" to='/tasks/create' v-if="this.$store.getters['getAccessToken'] != null">task create</v-btn>
-      </v-col>
+        </v-col>
+        <v-col cols="auto">
+            <v-btn outlined color="light-blue accent-4" to='/tasks/create' v-if="this.$store.getters['getAccessToken'] != null">task create</v-btn>
+        </v-col>
 
     </v-row>
 
     <v-layout row wrap>
-      
-      <b-table small striped :items="tasks" :fields="keys" :fixed="true">
 
-        <!-- taskカラム -->
-        <template v-slot:cell(task)="data">
-          <div class="body-2">
-            <router-link v-bind:to="'/tasks/' + data.item.taskId" class="body-2">{{data.item.taskName}}</router-link>
-            
-        <!--            <br/>  -->      
-        <!--    <a class="my-2" v-for="(tag,i) in data.item.tags" v-bind:key="i">-->
-        <!--      <v-chip rounded outlined dark color="secondary" small v-bind:to="'topics/' + tag.topic_id + '/tasks'">{{tag.topic_name}}</v-chip>-->
-        <!--    </a>-->
-          </div>
-        </template>
+        <b-table small striped :items="tasks" :fields="keys" :fixed="true">
 
-        <!-- urlカラム -->
-        <template v-slot:cell(url)="data">
-            <a v-bind:href="data.item.url" target="_blank">#</a>
-        </template>
-        
-        <!-- estimationカラム -->
-        <template v-slot:cell(estimation)="data">
-          <div class="body-2">
-            {{data.value}}
-          </div>
-        </template>
-      
-      </b-table>
+            <!-- taskカラム -->
+            <template v-slot:cell(task)="data">
+                <div class="body-2">
+                    <router-link v-bind:to="'/tasks/' + data.item.taskId" class="body-2">{{data.item.taskName}}</router-link>
+
+                    <!--            <br/>  -->
+                    <!--    <a class="my-2" v-for="(tag,i) in data.item.tags" v-bind:key="i">-->
+                    <!--      <v-chip rounded outlined dark color="secondary" small v-bind:to="'topics/' + tag.topic_id + '/tasks'">{{tag.topic_name}}</v-chip>-->
+                    <!--    </a>-->
+                </div>
+            </template>
+
+            <!-- urlカラム -->
+            <template v-slot:cell(url)="data">
+                <a v-bind:href="data.item.url" target="_blank">#</a>
+            </template>
+
+            <!-- estimationカラム -->
+            <template v-slot:cell(estimation)="data">
+                <div class="body-2">
+                    {{data.value}}
+                </div>
+            </template>
+
+        </b-table>
     </v-layout>
-  </v-container>
+</v-container>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-  data () {
-    return {
-      tasks : [],
-      task  : [],
-      host : process.env.VUE_APP_NINGENME_API_HOST,
-      path : process.env.VUE_APP_NINGENME_API_PATH,
-      keys: [
-        {key:'task'      , sortable: false, thStyle: { backgroundColor: '#d8bfd8',}},
-        {key:'url'       , sortable: false, thStyle: { backgroundColor: '#d8bfd8', width: "30px", }},
-        {key:'estimation', sortable: true , thStyle: { backgroundColor: '#d8bfd8', width: "120px", }},
-      ],
+    data() {
+        return {
+            tasks: [],
+            task: [],
+            host: process.env.VUE_APP_NINGENME_API_HOST,
+            path: process.env.VUE_APP_NINGENME_API_PATH,
+            keys: [{
+                    key: 'task',
+                    sortable: false,
+                    thStyle: {
+                        backgroundColor: '#d8bfd8',
+                    }
+                },
+                {
+                    key: 'url',
+                    sortable: false,
+                    thStyle: {
+                        backgroundColor: '#d8bfd8',
+                        width: "30px",
+                    }
+                },
+                {
+                    key: 'estimation',
+                    sortable: true,
+                    thStyle: {
+                        backgroundColor: '#d8bfd8',
+                        width: "120px",
+                    }
+                },
+            ],
 
-    }
-  },
-  mounted () {
-    axios
-      .get(this.host + this.path + '/tasks')
-      .then(response => (this.tasks = response.data.comproCategoryTaskList))
-      .catch(err => {console.log('err:', err);})
-  },
+        }
+    },
+    mounted() {
+        axios
+            .get(this.host + this.path + '/tasks')
+            .then(response => (this.tasks = response.data.comproCategoryTaskList))
+            .catch(err => {
+                console.log('err:', err);
+            })
+    },
 }
 </script>
--- src/views/topic/TopicIndex.vue
+++ src/views/topic/TopicIndex.vue
@@ -1,78 +1,86 @@
 <template>
-  <v-container>
+<v-container>
 
-    
     <!-- edit,create button -->
     <v-row>
-      <v-col
-        cols="auto"
-        class="mr-auto"
-      >
-        <v-btn outlined color="yellow darken-3" disabled >topic</v-btn>
-        <v-btn outlined color="yellow darken-3">
-        <select v-model="selected_topic">
-          <option v-bind:value="topic" v-for="(topic,i) in topics" v-bind:key="i">
-            {{ topic.topic_name}}
-          </option>
-        </select>
-        ▼
-        </v-btn>
-        <v-btn outlined color="yellow darken-3" v-on:click='prepareTopicEdit(selected_topic)' v-if="$store.getters['getAccessToken'] != null">edit</v-btn>
-        <!-- <v-btn outlined color="yellow darken-3" v-bind:to = "'/topics/' + selected_topic.topic_id + '/edit'" v-if="$store.getters['getAccessToken'] != null">edit</v-btn> -->
-        <v-btn outlined color="yellow darken-3" v-bind:to = "'/topics/' + selected_topic.topic_id + '/tasks'" v-if="$store.getters['getAccessToken'] == null">tasks</v-btn>
+        <v-col cols="auto" class="mr-auto">
+            <v-btn outlined color="yellow darken-3" disabled>topic</v-btn>
+            <v-btn outlined color="yellow darken-3">
+                <select v-model="selected_topic">
+                    <option v-bind:value="topic" v-for="(topic,i) in topics" v-bind:key="i">
+                        {{ topic.topicName}}
+                    </option>
+                </select>
+                ▼
+            </v-btn>
+            <v-btn outlined color="yellow darken-3" v-on:click='prepareTopicEdit(selected_topic)' v-if="$store.getters['getAccessToken'] != null">edit</v-btn>
+            <!-- <v-btn outlined color="yellow darken-3" v-bind:to = "'/topics/' + selected_topic.topic_id + '/edit'" v-if="$store.getters['getAccessToken'] != null">edit</v-btn> -->
+            <v-btn outlined color="yellow darken-3" v-bind:to="'/topics/' + selected_topic.topicId + '/tasks'" v-if="$store.getters['getAccessToken'] == null">tasks</v-btn>
 
-      </v-col>
+        </v-col>
 
-      <v-col cols="auto">
-        <v-btn outlined color="light-blue accent-4" to='/topics/create' v-if="this.$store.getters['getAccessToken'] != null">topic create</v-btn>
-      </v-col>
+        <v-col cols="auto">
+            <v-btn outlined color="light-blue accent-4" to='/topics/create' v-if="this.$store.getters['getAccessToken'] != null">topic create</v-btn>
+        </v-col>
 
     </v-row>
 
-
     <v-card>
-      <b-table small striped :items="topics" :fields="keys" :fixed="true">
-        <!-- genreカラム -->
-        <template v-slot:cell(genre)="data">
-          <v-chip rounded outlined dark color="cyan darken-4" small v-bind:to="'genres/' + data.item.label + '/topics'">{{data.item.genre_name}}</v-chip>
-        </template>
-        <!-- topicカラム -->
-        <template v-slot:cell(topic)="data">
-          <v-chip rounded outlined dark color="secondary" small v-bind:to="'topics/' + data.item.topic_id + '/tasks'">{{data.item.topic_name}}</v-chip>
-        </template>
-      </b-table>
+        <b-table small striped :items="topics" :fields="keys" :fixed="true">
+            <!-- genreカラム -->
+            <template v-slot:cell(genre)="data">
+                <v-chip rounded outlined dark color="cyan darken-4" small v-bind:to="'genres/' + data.item.label + '/topics'">{{data.item.genreName}}</v-chip>
+            </template>
+            <!-- topicカラム -->
+            <template v-slot:cell(topic)="data">
+                <v-chip rounded outlined dark color="secondary" small v-bind:to="'topics/' + data.item.topic_id + '/tasks'">{{data.item.topicName}}</v-chip>
+            </template>
+        </b-table>
     </v-card>
 
-  </v-container>
+</v-container>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-  data () {
-    return {
-      topics : [],
-      selected_topic: [],
-      urlPrefixComproCategoryAPI : process.env.VUE_APP_URL_PREFIX_COMPRO_CATEGORY_API,
-      keys: [
-        {key:'genre', sortable: false, thStyle: { backgroundColor: '#d8bfd8', width: "120px", }},
-        {key:'topic', sortable: false, thStyle: { backgroundColor: '#d8bfd8',}},
-      ],
+    data() {
+        return {
+            topics: [],
+            selected_topic: [],
+            host: process.env.VUE_APP_NINGENME_API_HOST,
+            path: process.env.VUE_APP_NINGENME_API_PATH,
+            keys: [{
+                    key: 'genre',
+                    sortable: false,
+                    thStyle: {
+                        backgroundColor: '#d8bfd8',
+                        width: "120px",
+                    }
+                },
+                {
+                    key: 'topic',
+                    sortable: false,
+                    thStyle: {
+                        backgroundColor: '#d8bfd8',
+                    }
+                },
+            ],
 
+        }
+    },
+    mounted() {
+        axios
+            .get(this.host + this.path + '/topics')
+            .then(response => (this.topics = response.data.comproCategoryTopicList))
+    },
+    methods: {
+        prepareTopicEdit(topic) {
+            this.$store.dispatch('setTopic', topic)
+            this.$router.push('/topics/' + topic.topicId + '/edit')
+        }
     }
-  },
-  mounted () {
-    axios
-      .get(this.urlPrefixComproCategoryAPI + '/topics')
-      .then(response => (this.topics = response.data))
-  },
-  methods : {
-    prepareTopicEdit(topic){
-      this.$store.dispatch('setTopic',topic)
-      this.$router.push('/topics/' + topic.topic_id + '/edit')
-    }
-  }
 
 }
 </script>
